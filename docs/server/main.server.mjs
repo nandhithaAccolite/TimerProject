c276ec85469dc5ee21185b4a8ee12502aaa89e31
Linga Nandhitha <linga.nandhitha@accolite.com>
2024-01-08 10:35:31 +0530

    initial commit

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LLTC6FW7.mjs";import"./chunk-N7SETSAJ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,71 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'TimerProject';
+
+  title = 'Timer Project';
+  currentPlay: boolean = false;
+  startPlay: boolean = false;
+  timeLeft: number = 300;
+  interval: any;
+
+  startTimer() {
+    this.currentPlay=true;
+    this.startPlay = true;
+    this.timeLeft = 300;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+        console.log(this.timeLeft);
+      } else {
+        this.currentPlay = false;
+        clearInterval(this.interval);
+        
+      }
+    }, 1000);
+  }
+
+  pauseTimer() {
+    this.currentPlay = false;
+    console.log("Timer paused");
+    clearInterval(this.interval);
+  }
+
+  resumeTimer() {
+    this.currentPlay = true;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+        console.log(this.timeLeft);
+      } else {
+        this.currentPlay = false;
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  resetTimer() {
+    this.currentPlay = false;
+    this.startPlay = false;
+    console.log("reset timer")
+    clearInterval(this.interval);
+    this.timeLeft = 300; 
+  }
+
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${this.padZero(seconds)}`;
+  }
+  padZero(value: number): string {
+    return value < 10 ? '0' + value : '' + value;
+  }
+
+  
 }
+
+
+
+
+
+
+
